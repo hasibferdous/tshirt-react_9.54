a0bf90680586291ca22aaed4f2a3585a0cdb3a8d
hasibferdous <hasibferdous.ict@gmail.com>
2022-10-08 13:50:53 +0600

    first commit

---
+++ src/_steps.js
@@ -0,0 +1,25 @@
+/**
+ * BASIC ROUTER SETUP:
+ * 
+ * 1. yarn add react-router-dom
+ * 2. create a router using createBrowserRouter
+ * 3. add RouterProvider and set router props (at app.js)
+ * 4. create a lot of components(home,orders,header, main)
+ * 5. create main layout with some common part and one changing part based on the route
+ * 6. set main layout at the root of the route(in Main.js -> Header,Outlet), (in App.js -> Main)
+ * 7. set children routes(      
+ *      {
+      path: '/',element: <Main></Main>, 
+      children:[
+        {path: '/',element:<Home></Home>},
+        {path: '/orders',element:<Orders></Orders>}
+                ]
+        }
+      )
+ * 8. set links on the header components (
+            <Link to='/'>Home</Link>
+            <Link to='/orders'>Orders</Link>
+        )
+
+    9.
+ */
